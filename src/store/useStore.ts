bd291966e5a82aa95cbca533ae5cef7efd969774
Maksym <87162463+maksy23@users.noreply.github.com>
2025-10-04 16:45:18 -0400

    feat: configure easy peasy store

---
+++ src/store/useStore.ts
@@ -0,0 +1,25 @@
+import { createStore, createTypedHooks } from 'easy-peasy'
+import type { Store } from 'easy-peasy'
+import { weatherModel, type WeatherModel } from './models/weatherModel'
+
+export interface StoreModel {
+  weather: WeatherModel
+}
+
+const model: StoreModel = {
+  weather: weatherModel(),
+}
+
+// Create the store
+const store: Store<StoreModel> = createStore(model)
+
+// Create typed hooks
+const typedHooks = createTypedHooks<StoreModel>()
+
+// Export the typed hooks
+// Use these instead of the hooks from the easy-peasy package in components
+export const UseStoreActions = typedHooks.useStoreActions
+export const UseStoreState = typedHooks.useStoreState
+
+// Export the store as default
+export default store
